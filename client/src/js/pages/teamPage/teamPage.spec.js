a74249c561192d2abb5089586750cba4fd5e84e6
bam-erfanian <erfanianbamdad@gmail.com>
2019-09-03 19:39:06 +1000

    [Bamdad][#0] Add name Bamdad to team page

--- client/src/js/pages/teamPage/teamPage.spec.js
+++ client/src/js/pages/teamPage/teamPage.spec.js
@@ -1,17 +1,21 @@
-const TeamPage = require('./teamPage');
+const TeamPage = require("./teamPage");
 
-describe('TeamPage', () => {
+describe("TeamPage", () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-    watchFace = document.getElementById('watch-face');
+    watchFace = document.getElementById("watch-face");
   });
 
-  describe('#render', () => {
-    it('should render my team page', () => {
+  describe("#render", () => {
+    it("should render my team page", () => {
       const page = new TeamPage();
       expect(page.render()).toContain("<h1>Made by:</h1>");
     });
   });
 
+  it("should contain team members names", () => {
+    const page = new TeamPage();
+    expect(page.render()).toContain("Bamdad");
+  });
 });
