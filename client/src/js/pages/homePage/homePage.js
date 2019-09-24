dd0fdba3db50f7b1b042c1b72cadaa19db84559b
Martin Liu <liuyinhsiang@Martins-MacBook-Pro.local>
2019-09-24 20:47:14 +1000

    added random numbers

--- client/src/js/pages/homePage/homePage.js
+++ client/src/js/pages/homePage/homePage.js
@@ -1,65 +1,65 @@
 const BasePage = require('watch-framework').BasePage;
 const StorageHub = require('watch-framework').StorageHub;
 const AudioHub = require('watch-framework').AudioHub;
-const logo = require('../../../images/logo.png');
+const logo = require('../../../images/donutcat.gif');
 const plop = './sounds/plop.mp3';
 
 //test for raspberrypi
 class HomePage extends BasePage {
-  template = require('./homePage.hbs');
+	template = require('./homePage.hbs');
 
-  pageWillLoad() {
-    StorageHub.setData('contacts', [
-      { name: 'Ray', phoneNumber: '0431 111 111' },
-      { name: 'Sinan', phoneNumber: '0431 222 222' },
-      { name: 'Jafari', phoneNumber: '0431 333 333' },
-    ])
+	pageWillLoad() {
+		StorageHub.setData('contacts', [
+			{ name: 'Ray', phoneNumber: '0431 111 111' },
+			{ name: 'Sinan', phoneNumber: '0431 222 222' },
+			{ name: 'Jafari', phoneNumber: '0431 333 333' }
+		]);
 
-    this.updateTimeEverySecond();
-    const dateTime = this.getDateTime();
-    this.date = dateTime.date;
-    this.time = dateTime.time;
-    this.logo = logo;
-  }
+		this.updateTimeEverySecond();
+		const dateTime = this.getDateTime();
+		this.date = dateTime.date;
+		this.time = dateTime.time;
+		this.logo = logo;
+	}
 
-  getDateTime() {
-    const dateTime = new Date(Date.now()).toLocaleString('en-AU').split(",");
-    return {
-      date: dateTime[0],
-      time: dateTime[1],
-    };
-  }
+	getDateTime() {
+		const dateTime = new Date(Date.now()).toLocaleString('en-AU').split(',');
+		return {
+			date: dateTime[0],
+			time: dateTime[1]
+		};
+	}
 
-  updateTimeEverySecond() {
-    setInterval(() => this.updateTimeDisplay(this.getDateTime), 1000);
-  }
+	updateTimeEverySecond() {
+		setInterval(() => this.updateTimeDisplay(this.getDateTime), 1000);
+	}
 
-  updateTimeDisplay(getTime) {
-    const clockTime = document.getElementById("clock-time");
-    if (clockTime) {
-      clockTime.textContent = getTime().time;
-    }
-  }
+	updateTimeDisplay(getTime) {
+		const clockTime = document.getElementById('clock-time');
+		if (clockTime) {
+			clockTime.textContent = getTime().time;
+		}
+	}
 
-  rightButtonEvent() {
-    this.navigate('contacts');
-  }
+	rightButtonEvent() {
+		this.navigate('contacts');
+	}
 
-  leftButtonEvent() {
-    AudioHub.playSound(plop);
-  }
+	leftButtonEvent() {
+		AudioHub.playSound(plop);
+	}
 
-  topButtonEvent() {
-    this.watchFace.scrollTop -= 40;
-  }
+	topButtonEvent() {
+		this.watchFace.scrollTop -= 40;
+	}
 
-  bottomButtonEvent() {
-    this.watchFace.scrollTop += 40;
-  }
+	bottomButtonEvent() {
+		this.watchFace.scrollTop += 40;
+	}
 
-    faceButtonEvent() {
-    this.navigate('demo');
-  }
+	faceButtonEvent() {
+		this.navigate('demo');
+	}
 }
 
 module.exports = HomePage;
