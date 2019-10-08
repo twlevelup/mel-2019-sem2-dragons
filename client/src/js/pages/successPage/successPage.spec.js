9d7ee5526290f9b9ea3caafec23462bb2ab8ed88
Martin Liu <liuyinhsiang@Martins-MacBook-Pro.local>
2019-10-08 20:39:39 +1100

    Fix the interface [0]

--- client/src/js/pages/successPage/successPage.spec.js
+++ client/src/js/pages/successPage/successPage.spec.js
@@ -1,19 +1,18 @@
-const successPage = require("./successPage");
-const AudioHub = require('watch-framework').AudioHub
+const successPage = require('./successPage');
+const AudioHub = require('watch-framework').AudioHub;
 
-describe("#success", () => {
-  it("should render the happy face png", () => {
-    const result = new successPage();
-    expect(result.render()).toContain(`<img id="img" src="../../../images/happy-face.png" height="100">`);
-  });
+describe('#success', () => {
+	it('should render the happy face png', () => {
+		const result = new successPage();
+		expect(result.render()).toContain(`<img id="successImg" src="../../../images/happy-face.png" height="100">`);
+	});
 });
 
-describe("#success sound", () => {
-  it("should play success sound", () => {
-    spyOn(AudioHub, "playSound");
-    const page = new successPage();
-    page.playSound();
-    expect(AudioHub.playSound).toHaveBeenCalledTimes(1);
-  });
-
+describe('#success sound', () => {
+	it('should play success sound', () => {
+		spyOn(AudioHub, 'playSound');
+		const page = new successPage();
+		page.playSound();
+		expect(AudioHub.playSound).toHaveBeenCalledTimes(1);
+	});
 });
