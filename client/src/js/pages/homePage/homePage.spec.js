744308442114c4e624197178520701d6445a66e9
sum <sathitsak@gmail.com>
2019-10-08 19:49:15 +1100

    Clean up [3]

--- client/src/js/pages/homePage/homePage.spec.js
+++ client/src/js/pages/homePage/homePage.spec.js
@@ -1,137 +1,101 @@
-const HomePage = require('./homePage');
-const StorageHub = require('watch-framework').StorageHub;
-const AudioHub = require('watch-framework').AudioHub;
+const HomePage = require("./homePage");
+const StorageHub = require("watch-framework").StorageHub;
+const AudioHub = require("watch-framework").AudioHub;
 
-describe('HomePage', () => {
-	let watchFace;
-	beforeEach(() => {
-		document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-		watchFace = document.getElementById('watch-face');
-	});
+describe("HomePage", () => {
+  let watchFace;
+  beforeEach(() => {
+    document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
+    watchFace = document.getElementById("watch-face");
+  });
 
-	describe('#pageWillLoad', () => {
-		it('should set contacts data on page load', () => {
-			spyOn(StorageHub, 'setData');
-			const page = new HomePage();
-			page.pageWillLoad();
-			expect(StorageHub.setData).toBeCalledWith('contacts', expect.any(Array));
-		});
-	});
+  describe("#pageWillLoad", () => {
+    it("should set contacts data on page load", () => {
+      spyOn(StorageHub, "setData");
+      const page = new HomePage();
+      page.pageWillLoad();
+      expect(StorageHub.setData).toBeCalledWith("contacts", expect.any(Array));
+    });
+  });
 
-	describe('#render', () => {
-		it('should render the username', () => {
-			const page = new HomePage();
-			expect(page.render()).toContain(`<h1>Hello, Kiddo!</h1>`);
-		});
-	});
+  describe("#render", () => {
+    it("should render the username", () => {
+      const page = new HomePage();
+      expect(page.render()).toContain(`<h1>Hello, Kiddo!</h1>`);
+    });
+  });
 
-	describe('#render', () => {
-		it('should render the cute character', () => {
-			const page = new HomePage();
-			const string = expect(page.render()).toContain(`<img src="../../../images/donutcat.gif" height="100">`);
-		});
-	});
+  describe("#render", () => {
+    it("should render the cute character", () => {
+      const page = new HomePage();
+      const string = expect(page.render()).toContain(
+        `<img src="../../../images/donutcat.gif" height="100">`
+      );
+    });
+  });
 
-	describe('#leftButtonEvent', () => {
-		it('audioHub plays a sound', () => {
-			spyOn(AudioHub, 'playSound');
-			const page = new HomePage();
-			page.leftButtonEvent();
-			expect(AudioHub.playSound).toBeCalledWith('./sounds/plop.mp3');
-		});
-	});
+  describe("#leftButtonEvent", () => {
+    it("audioHub plays a sound", () => {
+      spyOn(AudioHub, "playSound");
+      const page = new HomePage();
+      page.leftButtonEvent();
+      expect(AudioHub.playSound).toBeCalledWith("./sounds/plop.mp3");
+    });
+  });
 
-	describe('#faceButtonEvent', () => {
-		it('should take the user to the demo page', () => {
-			const props = {
-				navigate: () => {}
-			};
+  describe("#bottomButtonEvent", () => {
+    it("scrolls page down", () => {
+      const page = new HomePage({ watchFace });
 
-			const page = new HomePage(props);
-			spyOn(page, 'navigate');
+      page.bottomButtonEvent();
 
-			page.faceButtonEvent();
-			expect(page.navigate).toHaveBeenCalledWith('demo');
-		});
-	});
+      expect(watchFace.scrollTop).toEqual(40);
+    });
+  });
 
-	describe('#rightButtonEvent', () => {
-		it('goes to contacts page', () => {
-			const page = new HomePage();
-			spyOn(page, 'navigate');
+  describe("#topButtonEvent", () => {
+    it("scrolls page up", () => {
+      const page = new HomePage({ watchFace });
 
-			page.rightButtonEvent();
-			expect(page.navigate).toHaveBeenCalledWith('contacts');
-		});
-	});
+      page.topButtonEvent();
 
-	describe('#bottomButtonEvent', () => {
-		it('scrolls page down', () => {
-			const page = new HomePage({ watchFace });
+      expect(watchFace.scrollTop).toEqual(-40);
+    });
+  });
 
-			page.bottomButtonEvent();
+  describe("#updateTimeDisplay", () => {
+    it("updateTimeDisplays calls clock-time if its in the window", () => {
+      const page = new HomePage();
 
-			expect(watchFace.scrollTop).toEqual(40);
-		});
-	});
+      watchFace.innerHTML = page.render();
 
-	describe('#topButtonEvent', () => {
-		it('scrolls page up', () => {
-			const page = new HomePage({ watchFace });
+      jest.spyOn(page, "getDateTime");
+      page.updateTimeDisplay(page.getDateTime);
+      expect(page.getDateTime).toHaveBeenCalledTimes(1);
+    });
+  });
 
-			page.topButtonEvent();
+  describe("#updateTimeDisplay", () => {
+    it("updateTimeDisplays does not call clock-time if its not in the window", () => {
+      const page = new HomePage();
 
-			expect(watchFace.scrollTop).toEqual(-40);
-		});
-	});
+      jest.spyOn(page, "getDateTime");
+      page.updateTimeDisplay(page.getDateTime);
+      expect(page.getDateTime).toHaveBeenCalledTimes(0);
+    });
+  });
 
-	describe('#faceButtonEvent', () => {
-		it('should take the user to the demo page', () => {
-			const props = {
-				navigate: () => {}
-			};
+  describe("#updateTimeEverySecond", () => {
+    it("update time display gets called three times in 3000 ms", () => {
+      const page = new HomePage();
 
-			const page = new HomePage(props);
-			spyOn(page, 'navigate');
+      spyOn(page, "updateTimeDisplay");
 
-			page.faceButtonEvent();
-			expect(page.navigate).toHaveBeenCalledWith('demo');
-		});
-	});
+      jest.useFakeTimers();
+      page.updateTimeEverySecond();
+      jest.runTimersToTime(3000);
 
-	describe('#updateTimeDisplay', () => {
-		it('updateTimeDisplays calls clock-time if its in the window', () => {
-			const page = new HomePage();
-
-			watchFace.innerHTML = page.render();
-
-			jest.spyOn(page, 'getDateTime');
-			page.updateTimeDisplay(page.getDateTime);
-			expect(page.getDateTime).toHaveBeenCalledTimes(1);
-		});
-	});
-
-	describe('#updateTimeDisplay', () => {
-		it('updateTimeDisplays does not call clock-time if its not in the window', () => {
-			const page = new HomePage();
-
-			jest.spyOn(page, 'getDateTime');
-			page.updateTimeDisplay(page.getDateTime);
-			expect(page.getDateTime).toHaveBeenCalledTimes(0);
-		});
-	});
-
-	describe('#updateTimeEverySecond', () => {
-		it('update time display gets called three times in 3000 ms', () => {
-			const page = new HomePage();
-
-			spyOn(page, 'updateTimeDisplay');
-
-			jest.useFakeTimers();
-			page.updateTimeEverySecond();
-			jest.runTimersToTime(3000);
-
-			expect(page.updateTimeDisplay).toHaveBeenCalledTimes(3);
-		});
-	});
+      expect(page.updateTimeDisplay).toHaveBeenCalledTimes(3);
+    });
+  });
 });
