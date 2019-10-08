f0a56b17f3e96b237b0619f831420f674fc9be33
Martin Liu <liuyinhsiang@Martins-MacBook-Pro.local>
2019-10-08 19:37:16 +1100

    Clean up [2]

--- client/src/js/pages/finalPage/finalPage.js
+++ client/src/js/pages/finalPage/finalPage.js
@@ -1,8 +1,7 @@
-const BasePage = require('watch-framework').BasePage;
+const BasePage = require('../customBasePage/CustomBasePage');
 
 class FinalPage extends BasePage {
-  template = require('./finalPage.hbs');
-
+	template = require('./finalPage.hbs');
 }
 
 module.exports = FinalPage;
